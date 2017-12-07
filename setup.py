6c566b0902ee90f9c830bceb2af8cdb5522e4db7
robox <root@roboxv.pro>
2017-12-07 10:54:03 +0600

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='aocframework',
-      version='0.1',
+      version='0.2',
       description='Framework for solving tasks at adventofcode.com',
       url='https://github.com/roboxv/aocframework',
       author='robox',
